87c14605b40750c91f9271d1958fb37bb32711af
arnameti <arnameti@gmail.com>
2022-01-20 11:57:15 +0100

    implementing roll-dice-btn

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+scores = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.floor(Math.random() * 6) + 1;
+
+  diceEl.src = `dice-${dice}.png`;
+
+
+});
+
+
